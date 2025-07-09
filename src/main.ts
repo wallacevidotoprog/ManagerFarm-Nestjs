4b406822454b6204c3041d11a25a98448d6b4aee
WallaceVidoto <wallacevidoto.tech@gmail.com>
2025-07-09 13:19:35 -0300

    c dp

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
+import { AppModule } from './app.module';
+import { AllExceptionsFilter, ResponseInterceptor } from './common/interceptors/response.interceptor';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalInterceptors(new ResponseInterceptor());
+  app.useGlobalFilters(new AllExceptionsFilter());
+  app.enableCors({
+    origin: ['http://localhost:4200', 'https://manager-farm-angular.vercel.app'],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Accept', 'Authorization', 'Origin'],
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  app.use(cookieParser());
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
